b42c931fa6d7f4d9cafc00c4b7540eafca57d661
roff-dev <kieronoates@hotmail.co.uk>
2025-04-11 11:00:24 +0100

    created company controller and view templates

---
+++ src/AdminDashboard/Controllers/EmployeesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AdminDashboard.Data;
+using AdminDashboard.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminDashboard.Controllers
+{
+    public class EmployeesController
+    {
+    }
+}
